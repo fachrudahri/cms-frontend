f8ba42557dba5210d7b6c38bef2fc12193b7a8d2
fachrudahri <fachru.dahri@gmail.com>
2019-05-16 22:34:34 +0700

    new package

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,55 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
+
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+
+    <h1>Selamat Datang di Blog Gatsby with Strapi CMS</h1>
+    <p>dapatkan seputar pengetahuan tentang teknologi dan bahasa pemrograman disini</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <ul>
+      {data.allStrapiArticle.edges.map(document => (
+        <li key = {document.node.id}>
+          <h2>
+            <Link to = {`/${document.node.id}`}>
+              {document.node.title}
+            </Link>
+          </h2>
+          <Img fixed = {document.node.image.childImageSharp.fixed}/>
+          <div class="boxs">
+          <p>{document.node.content}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allStrapiArticle {
+      edges {
+        node {
+          id
+          image {
+            childImageSharp {
+              fixed(width:300) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+          title
+          content
+        }
+      }
+    }
+  }
+`
