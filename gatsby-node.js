826ab62b160242c5cc26aa27139bcda95cea1916
fachrudahri <fachru.dahri@gmail.com>
2019-06-13 17:34:01 +0700

    bagian frontend

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,23 +17,24 @@
 const path = require(`path`);
 
 const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
-  // Query for nodes to use in creating pages.
+  // Query for article nodes to use in creating pages.
   resolve(
     graphql(request).then(result => {
       if (result.errors) {
         reject(result.errors)
       }
-      
+
       return result;
     })
   )
 });
 
+
 // Implement the Gatsby API “createPages”. This is called once the
 // data layer is bootstrapped to let plugins create pages from data.
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators;
-  
+
   const getArticles = makeRequest(graphql, `
     {
       allStrapiArticle {
@@ -56,7 +57,33 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
       })
     })
   });
-  
-  // Query for articles nodes to use in creating pages.
-  return getArticles;
+
+  const getAuthors = makeRequest(graphql, `
+    {
+      allStrapiUser {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+    `).then(result => {
+    // Create pages for each user.
+    result.data.allStrapiUser.edges.forEach(({ node }) => {
+      createPage({
+        path: `/authors/${node.id}`,
+        component: path.resolve(`src/templates/author.js`),
+        context: {
+          id: node.id,
+        },
+      })
+    })
+  });
+
+  // Queries for articles and authors nodes to use in creating pages.
+  return Promise.all([
+    getArticles,
+    getAuthors,
+  ])
 };
